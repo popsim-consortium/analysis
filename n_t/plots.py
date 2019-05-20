0c86fc08395691c412943931bf9a10890c1ab7ec
Jared Galloway <jaredgalloway07@gmail.com>
2019-05-20 10:57:42 -0700

    Updated plotting to align colors

--- n_t/plots.py
+++ n_t/plots.py
@@ -63,20 +63,10 @@ def plot_compound_msmc(infiles, outfile):
 def plot_all_ne_estimates(sp_infiles, smcpp_infiles, msmc_infiles, outfile,
                             model, n_samp, generation_time, pop_id = 0, steps=None):
 
-    #mass_migration_events = []
-    #for demo in self.demographic_events:
-    #if type(demo) == MassMigration:
-    #    mass_migration_objects.append(demo)
-    #    mass_migration_times.append(demo.time)
-    
     ddb = msprime.DemographyDebugger(**model.asdict())
     if steps is None:
         end_time = ddb.epochs[-2].end_time + 1000
         steps = np.linspace(1,end_time,end_time+1)
-        steps = np.linspace(1,end_time,10)
-    # pop_size = ddb.population_size_trajectory(steps=steps)
-    # if mm abouve, switch pops at relavent time.
-    # pop_size = pop_size[:, pop_id]
     num_samples = [0 for _ in range(ddb.num_populations)]
     num_samples[pop_id] = n_samp
     coal_rate, P = ddb.coalescence_rate_trajectory(steps=steps,
@@ -87,17 +77,15 @@ def plot_all_ne_estimates(sp_infiles, smcpp_infiles, msmc_infiles, outfile,
     # plot smcpp estimates
     for infile in smcpp_infiles:
         nt = pandas.read_csv(infile, usecols=[1, 2], skiprows=0)
-        line1, = ax[0].plot(nt['x'], nt['y'], alpha=1.0)
+        line1, = ax[0].plot(nt['x'], nt['y'], alpha=0.8)
     ax[0].plot(steps, 1/(2*coal_rate), c="black")
-    #ax[0].plot(steps, pop_size, c="#43464B")
     ax[0].set_title("smc++")
 
     # plot stairwayplot estimates
     for infile in sp_infiles:
         nt = pandas.read_csv(infile, sep="\t", skiprows=5)
-        line2, = ax[1].plot(nt['year'], nt['Ne_median'],alpha=1.0)
+        line2, = ax[1].plot(nt['year'], nt['Ne_median'],alpha=0.8)
     ax[1].plot(steps, 1/(2*coal_rate), c="black")
-    #ax[1].plot(steps, pop_size, c="#43464B")
     ax[1].set_title("stairwayplot")
 
     for i,sample_size in enumerate(num_msmc):
@@ -106,15 +94,13 @@ def plot_all_ne_estimates(sp_infiles, smcpp_infiles, msmc_infiles, outfile,
             samp = fn.split(".")[0]
             if(samp == sample_size):
                 nt = pandas.read_csv(infile, usecols=[1, 2], skiprows=0)
-                line3, = ax[2+i].plot(nt['x'], nt['y'],alpha=1.0)
+                line3, = ax[2+i].plot(nt['x'], nt['y'],alpha=0.8)
             
         ax[2+i].plot(steps, 1/(2*coal_rate), c="black")
-        #ax[2].plot(steps, pop_size, c="#43464B")
         ax[2+i].set_title(f"msmc, ({sample_size} samples)")
 
     for i in range(2+len(num_msmc)):
         ax[i].set(xscale="log", yscale="log")
         ax[i].set_xlabel("time (years ago)")
     ax[0].set_ylabel("population size")
-        #ax[i].legend((line1, line2, line3), ('smc++', 'stairwayplot', 'msmc'))
     f.savefig(outfile, bbox_inches='tight', alpha=0.8)
