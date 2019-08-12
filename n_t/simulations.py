ca43b7fcca44d6cf0451715ea0e2e04d847bd226
andrewkern <adkern@uoregon.edu>
2019-08-12 18:16:50 +0000

    adding support in Snakefile for generic demog models and flat genetic maps

--- n_t/simulations.py
+++ n_t/simulations.py
@@ -10,11 +10,19 @@ def simulate(out_path, species, model, genetic_map, seed, chrmStr,
     chrom = species.genome.chromosomes[chrmStr]
     samples = [msp.Sample(population=population, time=0)] * sample_size
     print("Simulating...")
-    ts = msp.simulate(
-        samples=samples,
-        recombination_map=chrom.recombination_map(genetic_map.name),
-        mutation_rate=chrom.default_mutation_rate,
-        random_seed=seed,
-        **model.asdict())
+    if genetic_map:
+        ts = msp.simulate(
+            samples=samples,
+            recombination_map=chrom.recombination_map(genetic_map.name),
+            mutation_rate=chrom.default_mutation_rate,
+            random_seed=seed,
+            **model.asdict())
+    else:
+        ts = msp.simulate(
+            samples=samples,
+            recombination_map=chrom.recombination_map(genetic_map),
+            mutation_rate=chrom.default_mutation_rate,
+            random_seed=seed,
+            **model.asdict())
     ts.dump(out_path)
     print("Simulation finished!")
